a9acdbe7a6f7150f6114a0b98e19499aa5440d11
AlexanderSemenyak <42087193+AlexanderSemenyak@users.noreply.github.com>
2019-06-30 20:45:54 +0300

    Чуть по оптимизации скорости

--- Source/RuntimeNetCore/ZlpIOHelper.cs
+++ Source/RuntimeNetCore/ZlpIOHelper.cs
@@ -1480,7 +1480,7 @@
         [SecuritySafeCritical]
         public static long GetFileLength(string filePath)
         {
-            Trace.TraceInformation(@"About to get file length for path '{0}'.", filePath);
+            //Trace.TraceInformation(@"About to get file length for path '{0}'.", filePath);
 
             // 2014-06-10, Uwe Keim: Weil das auf 64-bit-Windows 8 nicht sauber läuft,
             // zunächst mal bei kürzeren Pfaden die eingebaute Methode nehmen.
